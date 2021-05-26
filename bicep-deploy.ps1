a8e340485d28bca1520c73e51444859bb8718715
tsukabon <shtsukam@outlook.com>
2021-05-26 12:26:52 +0900

    update demo env.

--- bicep-deploy.ps1
+++ bicep-deploy.ps1
@@ -1,10 +1,10 @@
-set-variable -name TENANT_ID "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx" -option constant
-set-variable -name SUBSCRIPTOIN_GUID "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx" -option constant
+set-variable -name TENANT_ID "72f988bf-86f1-41af-91ab-2d7cd011db47" -option constant
+set-variable -name SUBSCRIPTOIN_GUID "dc5d3c89-36dd-4a3c-b09b-e6ee41f6d5b5" -option constant
 set-variable -name BICEP_FILE "main.bicep" -option constant
 set-variable -name PARAMETER_FILE "azuredeploy.parameters.dev.json" -option constant
 
-$resourceGroupName = "xxxxx"
-$location = "xxxxx"
+$resourceGroupName = "azstudy-1-0526"
+$location = "japanwest"
 
 #Azure Login
 Connect-AzAccount -Tenant ${TENANT_ID} -Subscription ${SUBSCRIPTOIN_GUID}
